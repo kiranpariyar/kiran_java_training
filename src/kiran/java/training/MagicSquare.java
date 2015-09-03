7e9c6bf39c07b3bd7bfa9c825e9b05e892471e26
kiranpariyar <kirannpariyar@gmail.com>
2015-09-03 09:04:57 +0545

    implemented magic square problem checking program

---
+++ src/kiran/java/training/MagicSquare.java
@@ -0,0 +1,46 @@
+package kiran.java.training;
+
+public class MagicSquare {
+
+	public static void main(String[] args){
+		
+		int[] horizontalSum = new int[3];
+		int[] verticalSum = new int[3];
+		int[] diagonalSum = new int[2];
+		
+		int[][] magicSquare = new int[][]{
+								{2,7,6},
+								{9,5,1},
+								{4,3,8}
+							};					
+		int sum1, sum2;
+		//calculation of horizontal, vertical and diagonal sum
+		for( int i =0; i < magicSquare.length; i++){
+			
+			sum1 = 0;
+			sum2 = 0;
+			
+			for(int j=0; j < magicSquare[i].length; j++){
+				sum1 += magicSquare[i][j];
+				sum2 += magicSquare[j][i];
+				
+				if(i == j ){
+					diagonalSum[0] += magicSquare[i][j]; 
+				}
+				
+				if( i+j == 2){
+					diagonalSum[1] += magicSquare[i][j];
+				}
+			}
+			horizontalSum[i] = sum1;
+			verticalSum[i] = sum2;
+		}
+		
+		if( horizontalSum[0] == 15 && horizontalSum[1] == 15 && horizontalSum[2] == 15 && verticalSum[0] == 15 &&
+				verticalSum[1] == 15 && horizontalSum[2] == 15 && diagonalSum[0] == 15 && diagonalSum[1] == 15 ){
+			System.out.println("Magic Square");
+		}else{
+			System.out.println("Not Magic Square");
+		}
+	}
+}
---
+++ src/kiran/java/training/Simulation.java
@@ -0,0 +1,81 @@
+package kiran.java.training;
+
+import java.util.Random;
+
+public class Simulation {
+	
+	private double minST;
+	private double maxST;
+	private double serviceTime;
+	private double idleTime;
+	
+	public Simulation(){}
+	public Simulation(double min,double max){
+		this.minST = min;
+		this.maxST = max;
+		this.serviceTime = 0;
+		this.idleTime =0;
+	}
+	
+	public void setseviceTime(double serviceTime){
+		this.serviceTime = serviceTime;
+	}
+
+	public void setidleTime(double idleTime){
+		this.idleTime = idleTime;
+	}
+
+	public double calcServiceTime(double random){
+		return this.minST + (this.maxST-this.minST) * random;
+	}
+	
+	public static double generateRandom(){
+		Random random = new Random();
+		double randomnum = random.nextDouble();
+		return randomnum;
+	}
+	
+	public static void main(String[] args){
+		char eventType;
+		double arrivalrand;
+		double servicerandA = 0,servicerandB = 0;
+		double idletimeA = 0,idletimeB = 0;
+		double servicetimeA = 0,servicetimeB = 0;
+		int countA = 0,countB = 0,clock = 0;
+		
+		System.out.println("arrivalrand\t\tarrival\t\tservicerandA\tservicetimeA\t\t\tidletimeA");
+		while(countA < 50){
+			arrivalrand = generateRandom();
+			Simulation simA;
+			Simulation simB;
+			if(arrivalrand <=0.9){
+				eventType = 'A';
+				simA = new Simulation(3.445628284,5.54371716);
+				servicerandA = generateRandom();
+				servicetimeA = simA.calcServiceTime(servicerandA);
+				idletimeA = idletimeA + (6-servicetimeA);
+				simA.setidleTime(idletimeA);
+				if(generateRandom()<=0.9){
+					countA++;
+				}	
+			}else{
+				eventType = 'B';
+				simB = new Simulation(17.22814142,22.77185858);
+				servicerandB = generateRandom();
+				servicetimeB = simB.calcServiceTime(servicerandB);
+				idletimeB = idletimeB + (6-servicetimeB);
+				simB.setidleTime(idletimeB);
+				if(generateRandom()<=0.9){
+					countB++;
+					
+				}
+			}
+			clock += 6;
+			
+			System.out.println(arrivalrand+"\t"+eventType+"\t"+servicerandA+"\t"+
+			servicetimeA+"\t"+idletimeA);
+		}	
+		
+		System.out.println(idletimeA + " : " +clock);
+	}
+}
